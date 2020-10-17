50af200a4c9f638f0881519ef4f1fe938d9a2439
karas741 <71412848+karas741@users.noreply.github.com>
2020-10-17 19:18:15 +0900

    [코드 API 만들기 02-2] POST 방식 API 사용하기
    [코드 API 만들기 02-2] POST 방식 API 사용하기

--- projects/prac/app.py
+++ projects/prac/app.py
@@ -17,5 +17,12 @@ def test_get():
     return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
 
 
+@app.route('/test', methods=['POST'])
+def test_post():
+    title_receive = request.form['title_give']
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
